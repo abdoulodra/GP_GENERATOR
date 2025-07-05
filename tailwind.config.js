703a45ae0c1ca88b4d79b8a3b767b0f660254cf1
abdoulodra <139270059+abdoulodra@users.noreply.github.com>
2025-07-05 13:53:27 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,21 @@
 // Tailwind config
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
+    "./app/**/*.{js,jsx,ts,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neonBlue: '#4D96FF',
+        neonPurple: '#A259FF'
+      },
+      fontFamily: {
+        'futuristic': ['Orbitron', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
